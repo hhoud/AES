bea24c96d12902002893b4e443b3d91c74e9275a
Olivier Janssens <ojtwist@gmail.com>
2011-03-10 10:21:56 +0100

    AES-read any file

--- AES-encryptie/Form1.cs
+++ AES-encryptie/Form1.cs
@@ -11,14 +11,38 @@ namespace AES_encryptie
 {
     public partial class Form1 : Form
     {
+        //Global variables
+
+        //The extention of the loaded document
+        String ext;
+
+        //The converter class
+        Converter converter = new Converter();
+
+        //An object for the 1st step of an AES round
+        Substitution substitution = new Substitution();
+
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void btnGetData_Click(object sender, EventArgs e)
         {
+           // Set the title of the ofd
+           ofd.Title = "Select a file";
 
+           //Check if the user selected something
+           String file = (ofd.ShowDialog() == DialogResult.OK) ? ofd.FileName : "";
+
+           // Get the extention of the loaded file
+           ext =  System.IO.Path.GetExtension(file);
+
+           //Convert the file into an array of bytes
+           //1ste step = Substitution
+           substitution.substitute(converter.FileToByteArray(file));
+           
         }
     }
 }
---
+++ AES-encryptie/Substitution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AES_encryptie
+{
+    public class Substitution
+    {
+
+        public byte[] substitute(byte[] data) {
+            generateSBoxes();
+
+
+            return data;
+        }
+
+        // This function generates the S-Boxes
+        private byte[] generateSBoxes()
+        {
+            return null;
+        }
+
+    }
+}
