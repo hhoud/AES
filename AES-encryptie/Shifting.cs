0f8208989237bab57d101277899ad928509744fa
unknown <Henry@.(none)>
2011-03-14 17:43:20 +0100

    AES - Added row shifting code with comments, clean commit

---
+++ AES-encryptie/Shifting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AES_encryptie
+{
+    class Shifting
+    {
+        public byte[,] shiftRows(byte[,] data)
+        {
+            //Create an empty byte array which will contain the shifted matrix.
+            byte[,] shifted = new byte[4,4];
+
+            //Loop over the bytes in the matrix
+            for(int i=0; i<4 ; i++){
+                for (int j = 0; j < 4; j++)
+                {
+                    //Copy the bytes to the left depending on the row.
+                    //The first row doesn't change, the second is shifted one to the 
+                    //left and the first bytes are appended at the end of the row.
+                    shifted[i, j] = data[i, (i + j)%4];
+                }
+            }
+            return shifted;
+        }
+    }
+}
